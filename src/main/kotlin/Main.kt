d83e7842c00c4c74e0426bda84c32d15518ae01f
EstherMwiyeria <esthermkanoro@gmail.com>
2023-02-08 16:17:21 +0300

    added an interesting fact about myself

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+fun main(){
+   var Greeting = ("hello ")
+   name("Esther")
+   var division = divideNumbers(217,22)
+   println(division)
+   var sum = addFourNumbers(3876,490,254,6998)
+   println(sum)
+   println("I'm a pencil artist")
+
+
+}
+fun name(name: String){
+   var nme="Hello"
+   println("Hello"+ name)
+
+
+}
+fun divideNumbers(num1:Int,num2:Int):Int{
+   var division= num1 % num2
+   return division
+}
+fun addFourNumbers(num1: Int,num2: Int,num3: Int,num4: Int):Int{
+   var sum= num1 + num2 + num3+num4
+   return sum
+}
+fun intrestingFact(){
+   var iAm = "I'm a pencil artist"
+}
